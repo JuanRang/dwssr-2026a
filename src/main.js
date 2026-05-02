529363757ccd3d96142216e48eaca398f52fffc9
JuanRang <jrangelneri2@gmail.com>
2026-05-01 22:07:19 -0600

    feat: CSS Framework enhance project setup with Tailwind CSS integration, update scripts, and add header styles

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 //IMPORTAR UN ESTILO
-import './styles/main.css'
+import './styles/main.css';
+import './styles/header.css';
 
 //Scripts principales
 console.log("Vite +`Express + Handlebars funcionando")
